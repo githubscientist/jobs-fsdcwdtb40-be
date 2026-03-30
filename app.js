c4ef49d490c757d118986fb777a79f0929b50301
githubscientist <sathish.datascientist@gmail.com>
2026-03-30 20:36:26 +0530

    backend updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://jobs-fsdcwdtb40-fe.netlify.app',
     credentials: true,
 }));
 
